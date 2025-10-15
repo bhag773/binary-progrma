38d8ca537286566756ca39d6c5e0ceed4abddca6
bhag773 <gs05268348@priestley.ac.uk>
2025-10-15 10:15:13 +0100

    Add files via upload

---
+++ binary program.py
@@ -0,0 +1,16 @@
+def toBinary(integer):
+   # if integer % 2 == 1 :
+    #    bin1 = 1
+    #elif integer % 2 == 0:
+     #   bin2 = 0
+      #  binary = (bin1,bin2)
+       # return binary
+
+
+
+
+
+
+int_ = int(input("Enter a number between 1 and 255: "))
+
+print(toBinary(int_))
